91948a3727af99f50d831b610112a3353f756358
Yanelen <yanelen@yahoo.com>
2015-12-14 10:03:23 -0500

    added css and js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jumbotron-narrow.css bootstrap.min.css bootstrap.min.js ie-emulation-modes-warning.js ie10-viewport-bug-workaround.js )
